5cd9ea9964a730da3a3be4cb5c624b00d0ed429a
Jose Rubio <jadrubio@gmail.com>
2024-04-03 14:43:44 -0400

    directory restructure and reading input from cli

---
+++ numberinout/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int user_num;
+    cout << "Type in a number to read in: ";
+    cin >> user_num;
+    cout << "You typed the number " << user_num << endl;
+    return 0;
+}
